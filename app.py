8496fb61ccdfe6208299e1e7daa1712501e56eae
beasaab <biasaab18@gmail.com>
2023-02-10 16:05:49 +0100

    Second Session - App

--- app.py
+++ app.py
@@ -10,11 +10,8 @@ class App(Items):
     # a function within a class is called a method
     def filter(self):
         words = [self.entry_1.get(), self.entry_2.get(), self.entry_3.get(), self.entry_4.get()]
-
-
-
-
-
+        words_filtered = [x for x in words if x != ""]
+        print(words_filtered)
 
 
 app = App()
